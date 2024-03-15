20d8ff48f27afdfba003e639fc501d1fc2dd315e
Your Name <your.email@example.com>
2024-03-15 20:52:21 +0500

    js commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Reverse the string
+let originalString = "Superman";
+let reversedString = originalString.split('').reverse().join('');
+console.log("Reversed String:", reversedString);
+
+// Reverse the string Funtion method
+// let originalString = "Superman";
+// let reversedString = reverseString(originalString);
+// console.log(reversedString);
+// function reverseString(str) {
+//     let reversed = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+// Reverse the string loop  method
+// const str = "Superman";
+// let reversed = "";
+// for(let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+// }
+// console.log(reversed);
